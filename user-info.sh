e617cb1ab1eae316c1f551522e0b9512b132bd9d
Priyanshi Panigrahi <panigrahipriyanshi22@gmail.com>
2026-05-25 17:46:03 +0000

    Added user information automation script

---
+++ user-info.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "===== USER INFORMATION ====="
+
+echo ""
+
+echo "Current User:"
+whoami
+
+echo ""
+
+echo "User ID Information:"
+id
+
+echo ""
+
+echo "Home Directory:"
+echo $HOME
+
+echo ""
+
+echo "User Groups:"
+groups
